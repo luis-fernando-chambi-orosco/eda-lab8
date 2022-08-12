67264c2082693eb39949e0b3f3b0c487125494b7
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-08-11 23:34:44 -0500

    comiit prueba

---
+++ prueba.java
@@ -0,0 +1,21 @@
+public class prueba {
+	public static void main(String[] args) {
+BTree<Integer> a = new BTree<Integer>(3);
+
+        a.add(1);
+        a.add(2);
+        a.add(16);
+        a.add(54);
+        a.add(17);
+        a.add(69);
+        a.add(9);
+
+        System.out.println("Arbol:");
+        a.recorrido();
+        System.out.println("Eliminando elementos");
+        a.remove(54);
+        a.remove(69);
+        System.out.println("Arbol después de haber eliminado:");
+        a.recorrido();
+}
+}
